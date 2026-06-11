30b28eb2e4bc51c9025b38ddee0aa2e9d207d30a
Rudra Pratap Singh <rudransh.iitm@gmail.com>
2026-06-11 18:43:59 +0530

    Fix drawer overlapping status bar in iOS PWA by adding interactive-widget=resizes-visual

--- app/layout.tsx
+++ app/layout.tsx
@@ -25,6 +25,7 @@ export const viewport: Viewport = {
   initialScale: 1,
   maximumScale: 1,
   viewportFit: "cover",
+  interactiveWidget: "resizes-visual",
 };
 
 export default function RootLayout({
